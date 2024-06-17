34c2d671ad966307168e4a35871c7f33dff197b3
Jose Dominguez <utti222001@utvco.edu.mx>
2024-06-17 13:41:33 -0600

    lineas de ayuda violentometro

--- src/app/pages/comunicacion/comunicacion.module.ts
+++ src/app/pages/comunicacion/comunicacion.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ComunicacionPageRoutingModule } from './comunicacion-routing.module';
 
 import { ComunicacionPage } from './comunicacion.page';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ComunicacionPageRoutingModule
+    ComunicacionPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [ComunicacionPage]
 })
